1d12cec07bbfe89d69d76a3def97854a8f814c47
congtung10t2 <congtung10t2>
2017-12-30 15:59:42 +0800

    init project

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+import { CalendarPage } from '../calendar/calendar';
+import { ThisWeekPage } from '../this_week/this_week';
+import { TodayPage } from '../today/today';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab1Root = TodayPage;
+  tab2Root = CalendarPage;
+  tab3Root = ThisWeekPage;
 
   constructor() {
 
--- src/pages/today/today.ts
+++ src/pages/today/today.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: 'page-today',
+  templateUrl: 'today.html'
 })
-export class HomePage {
+export class TodayPage {
 
   constructor(public navCtrl: NavController) {
-
+    
   }
 
 }
