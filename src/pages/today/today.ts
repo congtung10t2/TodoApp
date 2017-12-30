3594996251a761cecc3b5f65b158cb78c7861d2d
congtung10t2 <congtung10t2>
2017-12-30 22:33:03 +0800

    Add new task

--- src/pages/today/today.ts
+++ src/pages/today/today.ts
@@ -1,14 +1,41 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { dataManager } from '../../data/dataManager';
 
 @Component({
   selector: 'page-today',
   templateUrl: 'today.html'
 })
 export class TodayPage {
-
+  isNewTask = false;
+  canAddNewTask = false;
+  inputText = "";
+  items = [];
   constructor(public navCtrl: NavController) {
+    this.items = dataManager;
+  }
+  itemSelected(item){
+
+  }
+  addTask(){
+    if(!this.isNewTask)
+    {
+      this.isNewTask = true;
+      this.canAddNewTask = false;
+    }
+      
+    else if(this.canAddNewTask){
+      this.items.push({text: this.inputText+"", done: false});
+      this.inputText = "";
+
+    }
     
+
+  }
+  onChangeText(value){
+    if(value.length > 0){
+      this.canAddNewTask = true;
+    }
   }
 
 }
